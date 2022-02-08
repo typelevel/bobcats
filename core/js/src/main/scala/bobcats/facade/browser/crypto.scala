e8876e2c20d5357219cc0c463879f924238ae852
Henry Story <henry.story@bblfish.net>
2022-02-08 19:36:48 +0100

    scala3 warning on @nowarn -> "never used"

--- core/js/src/main/scala/bobcats/facade/browser/crypto.scala
+++ core/js/src/main/scala/bobcats/facade/browser/crypto.scala
@@ -22,7 +22,7 @@ import scala.scalajs.js.annotation.JSGlobal
 
 @js.native
 @JSGlobal
-@nowarn("cat=unused")
+@nowarn("msg=never used")
 private[bobcats] object crypto extends js.Any {
 
   def subtle: SubtleCrypto = js.native
