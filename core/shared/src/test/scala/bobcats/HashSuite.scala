5796fba537838077572c440f65fc857fe0e243f3
Yilin Wei <yilin@kebab-ca.se>
2023-08-10 18:31:11 +0100

    Ignore for `MD5`.

--- core/shared/src/test/scala/bobcats/HashSuite.scala
+++ core/shared/src/test/scala/bobcats/HashSuite.scala
@@ -72,6 +72,6 @@ class HashSuite extends CryptoSuite {
     SHA512,
     hex"cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e")
 
-  testVector(MD5, hex"9e107d9d372bb6826bd81d3542a419d6", ignoredRuntimes = Set("Browser"))
+  testVector(MD5, hex"9e107d9d372bb6826bd81d3542a419d6", ignoredRuntimes = Set("Firefox", "Chrome"))
 
 }
